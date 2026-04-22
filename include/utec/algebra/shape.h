b8e1ff937e683ee292a0287ce37710041985cb79
BrandoVM <xdlolbrando123@gmail.com>
2026-04-22 18:52:44 -0500

    Implementacion de la clase Shape - Edinson

--- include/utec/algebra/shape.h
+++ include/utec/algebra/shape.h
@@ -1,6 +1,28 @@
 #ifndef PROG3_PF_EPIC1_FEATURE1_V2026_01_SHAPE_H
 #define PROG3_PF_EPIC1_FEATURE1_V2026_01_SHAPE_H
 
+#include <cstddef>
+#include <initializer_list>
+#include <stdexcept>
+#include <vector>
+
+class Shape {
+public:
+	Shape() = default;
+	Shape(std::initializer_list<int> dims); 
+	explicit Shape(std::vector<int> dims);
+	[[nodiscard]] std::size_t rank() const noexcept;
+	[[nodiscard]] std::size_t numel() const;
+	[[nodiscard]] const std::vector<int>& dims() const noexcept;
+	[[nodiscard]] int operator[](std::size_t i) const;
+	friend bool operator==(const Shape&, const Shape&) = default;
+
+private:
+	std::vector<int> dims_;
+	void validate() const;
+};
+
+namespace utec{}
 
 
 #endif //PROG3_PF_EPIC1_FEATURE1_V2026_01_SHAPE_H
