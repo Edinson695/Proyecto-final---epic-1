df113381da393ee771fdfd7bfcbc04a465b48852
Hector Choque Dueñas <hector.choque@utec.edu.pe>
2026-04-23 02:57:10 -0500

    avance

--- src/shape.cpp
+++ src/shape.cpp
@@ -1,37 +1,39 @@
 #include "utec/algebra/shape.h"
 
-void Shape::validate() const {
-    for (int dim : dims_) {
-        if (dim <= 0) {
-            throw std::invalid_argument("toda dimension debe ser positiva");
+namespace utec::tf {
+    void Shape::validate() const {
+        for (int dim : dims_) {
+            if (dim <= 0) {
+                throw std::invalid_argument("toda dimension debe ser positiva");
+            }
         }
     }
-}
 
-Shape::Shape(std::initializer_list<int> dims) : dims_(dims) {
-    validate();
-}
-
-Shape::Shape(std::vector<int> dims) : dims_(dims) {
-    validate();
-}
-
-size_t Shape::rank() const noexcept {
-    return dims_.size();
-}
-
-size_t Shape::numel() const {
-    std::size_t total = 1;
-    for (const int& d : dims_ ) {
-        total *= d;
+    Shape::Shape(std::initializer_list<int> dims) : dims_(dims) {
+        validate();
     }
-    return total;
-}
 
-const std::vector<int>& Shape::dims() const noexcept {
-    return dims_;
-}
+    Shape::Shape(std::vector<int> dims) : dims_(dims) {
+        validate();
+    }
 
-int Shape::operator[](std::size_t i) const {
-    return dims_[i];
+    size_t Shape::rank() const noexcept {
+        return dims_.size();
+    }
+
+    size_t Shape::numel() const {
+        std::size_t total = 1;
+        for (const int& d : dims_ ) {
+            total *= d;
+        }
+        return total;
+    }
+
+    const std::vector<int>& Shape::dims() const noexcept {
+        return dims_;
+    }
+
+    int Shape::operator[](std::size_t i) const {
+        return dims_[i];
+    }
 }
---
+++ src/tensor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jmreyes on 22/04/2026.
+//
+#include "utec/algebra/tensor_backend.h"
+
